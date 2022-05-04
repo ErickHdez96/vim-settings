9753700f143a029b8e4b98355bebc02044055692
Erick Hernandez Curiel <erick@hernandezcuriel.dev>
2022-05-04 12:23:58 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+set -x;
+set -e;
+
 git submodule update --recursive --init
 mkdir -p $HOME/.local/share/nvim
 ln -s $HOME/.config/nvim/site $HOME/.local/share/nvim/site
+python3 -m venv $HOME/.local/share/nvim/venv
+$HOME/.local/share/nvim/venv/bin/pip install pynvim
